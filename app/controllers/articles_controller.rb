c7758a69a7df458564bf650346dd4da3baba1ec4
sheringugi <sheri.ngugi17@gmail.com>
2023-07-11 15:03:05 +0300

    Complete assignment

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,19 @@ class ArticlesController < ApplicationController
     render json: article
   end
 
+  def show_article
+    session[:page_views] ||= 0
+    session[:page_views] += 1
+  
+    if session[:page_views] < 3
+      # Render JSON response with article data
+      render json: { article: { id: params[:id], title: 'Sample Article' } }
+    else
+      # Render JSON response with error message and status code 401
+      render json: { error: 'You have reached the maximum number of page views.' }, status: 401
+    end
+  end
+
   private
 
   def record_not_found
